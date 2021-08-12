7ba4cc05f734c06360b5b1046f902f64106e529e
Lorena <lorena.perez@gmail.com>
2021-08-12 21:33:47 +0200

    add header, logo and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import Header from "./components/Header";
 import AddCity from "./components/AddCity";
 import City from "./components/City";
 import ListCities from "./components/ListCities";
@@ -9,20 +10,8 @@ import ListCities from "./components/ListCities";
 function App() {
   return (
     <>
-     <nav className="navbar">
-       <div className="navbar__list">
-         <li className="navbar__item">
-           <Link to={"/cities"} className="navbar__item--link">
-           Cities
-           </Link>
-         </li>
-         <li className="navbar__item">
-           <Link to={"/add"} className="navbar__item--link">
-           Add city
-           </Link>
-         </li>
-       </div>
-     </nav>
+    <Header/>
+    
      
      <div className="container mt-3">
           <Switch>
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "../stylesheet/header.css";
+import logo from "../images/logo-1.png";
+
+function Header() {
+    return (
+    <div className="header">
+        <a href="/">
+            <img className="header__logo" src={logo} alt="logo wefox travel"/>
+        </a>
+        <nav className="header__navbar">
+           <div className="header__navbar--list">
+             <li className="header__navbar--item">
+               <Link to={"/cities"} className="navbar__item--link">
+               Cities
+               </Link>
+             </li>
+             <li className="navbar__item">
+               <Link to={"/add"} className="navbar__item--link">
+               Add City
+               </Link>
+             </li>
+           </div>
+         </nav>
+    </div>
+    )
+}
+
+export default Header
